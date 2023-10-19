96f13afe8f9909b412a47fbf909a011909a182ff
aminbro <barrimohamed01@gmial.com>
2023-10-19 11:37:43 +0100

    first commiccgt

---
+++ text.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Document</title>
+
+    <style >
+
+body{
+    background-image: url(img1.jpg);
+}
+
+    </style>
+    
+</head>
+<body>
+    <div class="countainer">
+xkjcadbcahd
+    </div>
+
+
+<script src="jst.js"></script>
+</body>
+</html>
